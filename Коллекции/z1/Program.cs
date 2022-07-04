073b8b5ab314b2ec98fd1d97ab52220823da2732
Ricalii1 <107534177+Ricalii1@users.noreply.github.com>
2022-07-04 09:04:17 +0300

    Add files via upload

---
+++ Коллекции/z1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var d = new Dictionary<string, string>();
+                d.Add("музыка", "вид искусства, воплощенное через звуки");
+                d.Add("волейбол", "командный вид спорта с мячом");
+                d.Add("фэнтези", "жанр в литературе");
+                d.Add("наследование", "один из принципов ООП");
+                d.Add("яблоко", "плод яблони");
+                string s;
+                while (true)
+                {
+                    Console.WriteLine("\nВведите слово: ");
+                    s = Console.ReadLine().Trim().ToLower();
+                    if (d.ContainsKey(s))
+                        Console.WriteLine(d[s]);
+                    else
+                        Console.WriteLine("Слово отсутствует в словаре." + "\n");
+
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Коллекции/z2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Random r = new Random();
+
+                List<int> list = new List<int>();
+                Console.WriteLine("Количество клиентов: ");
+                int a = r.Next(1, 5);
+                Console.WriteLine(a + " ");
+                int[] arr = new int[a];
+
+                int bill = 0; //счет
+                Console.WriteLine("Сумма покупок: ");
+                for (int i = 0; i < a; i++)
+                {
+                    arr[i] += r.Next(10, 15);
+                    list.Add(arr[i]);
+                    Console.Write(list[i] + " ");
+                }
+
+                Console.WriteLine("\nОбслужить клиента? 1 - yes, 0 - no");
+                int p = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("bill:");
+                if (p == 0)
+                {
+                    Process.GetCurrentProcess().Kill();
+                } 
+                if (p == 1)
+                {
+                    foreach (int item in list)
+                    {
+                        bill += item;
+                        for (int i = 0; i < a; i++)
+                        {
+                            list.Remove(i);
+                        }
+                        
+                    }
+                    Console.WriteLine(bill + "");
+                    Console.WriteLine("\nСписок:");
+                    for (int i = 0; i < a; i++)
+                    {
+                        Console.Write(list[i] + " ");
+                    }
+                }
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
