9dc3fd4cdbb16d1c87e46f3f0c633a0e77d00319
Ricalii1 <107534177+Ricalii1@users.noreply.github.com>
2022-07-04 09:09:12 +0300

    Add files via upload

---
+++ Ввод вывод/z1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace z1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+                Directory.CreateDirectory($@"C:\\Folder_{i}");
+            Console.WriteLine("Директории созданы");
+
+            Console.WriteLine("Удалить директории?\ny - Да\tn - Нет");
+            string s = Console.ReadLine();
+
+            if (s == "y")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Directory.Delete($@"C:\\Folder_{i}");
+                }
+                Console.WriteLine("Директории удалены");
+            }
+            else if (s == "n")
+                Console.WriteLine("Директории в месте создания");
+        }
+    }
+}
---
+++ Ввод вывод/z2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace z2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\testt\test1.txt";
+            Console.WriteLine("Файл создан\n");
+
+            StreamWriter sw = new StreamWriter(path);
+            Console.WriteLine("Введите данные для файла: ");
+            sw.Write(Console.ReadLine());
+            sw.Close();
+
+            StreamReader sr = new StreamReader(@"C:\testt\test1.txt");
+            Console.WriteLine("\nСодержимое файла:");
+
+            for (int i = 0; i < File.ReadAllLines(@"C:\testt\test1.txt").Length; i++)
+                Console.WriteLine(sr.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
